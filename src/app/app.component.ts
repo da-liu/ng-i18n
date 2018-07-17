b4b0964fa3c3b33649fb5c70d4b1669abad80311
Da Liu <dluoft@gmail.com>
2018-07-17 09:03:48 -0400

    [ngx-translate] simple setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  param = { value: 'world' };
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    let browserLanguage = translate.getBrowserLang();
+    translate.use(browserLanguage.match(/en|fr/) ? browserLanguage : 'en');
+  }
+
+  changeLanguage(lang) {
+    this.translate.use(lang);
+    // console.log('changing language to ' + lang);
+    // console.log(this.translate.currentLang);
+  }
 }
